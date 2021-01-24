66a5a8e0ec57b9e24fc8206f8741b1f84bf669f4
Shusandesh GC <gcshusandesh@gmail.com>
2021-01-24 18:57:08 +0545

    initial commit

---
+++ android/app/src/main/kotlin/shusandesh/wakie_wakie/MainActivity.kt
@@ -0,0 +1,6 @@
+package shusandesh.wakie_wakie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Wakie')),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Add Alarm',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
